f8f97b3c2c8e4aeb1669c8fe79d614ad91a65d64
Sparshdeep Singh <66513733+Sparsh300@users.noreply.github.com>
2025-05-29 17:10:32 +1000

    Update tasks.test.js

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -1,10 +1,15 @@
 const request = require('supertest');
-const app = require('../app');
+const server = require('../app');  // adjust if your file structure is different
 
 describe('Task API', () => {
   it('GET /tasks should return empty list', async () => {
-    const res = await request(app).get('/tasks');
+    const res = await request(server).get('/tasks');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual([]);
   });
+});
+
+// Properly close the server after tests to prevent Jest from hanging
+afterAll((done) => {
+  server.close(done);
 });
